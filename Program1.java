479a752d639bf378b5c75d46098e0188a9e98d3b
NidhiGandhi123 <Nidhijgandhi1@gmail.com>
2023-01-15 21:25:37 +0000

    first commit

---
+++ Program1.java
@@ -0,0 +1,11 @@
+package NestedForloop;
+
+public class Program1 {
+    public static void main(String[] args) {
+        for(int i=1;i<=3;i++){
+            for(int j=1;j<=3;j++){
+                System.out.println(i+" "+j);
+            }
+        }
+    }
+}
---
+++ Program5.java
@@ -0,0 +1,12 @@
+package NestedForloop;
+
+public class Program5 {
+    public static void main(String[] args) {
+        for(int i=6;i<=10;i++){
+            for(int j=1;j<=10;j++){
+                System.out.println(i+" * " +j+ " = "+ i*j );
+            }
+            System.out.println("");
+        }
+    }
+}
